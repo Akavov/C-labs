befdef34cafb483d17abefc7685c36c90f518020
Arslan Akavov <arsakavov@gmail.com>
2018-02-21 01:12:56 +0300

    c rules

---
+++ README.md
@@ -0,0 +1,7 @@
+# Programming in C
+
+Here you will find a few interesting problems solved by me on labs in Bauman Moscow State Technical University (BMSTU) during my "Programming in C" course (4th semester).
+
+Each folder consists of assignment description and solution (writen on C).
+
+C-labs / BMSTU 4 semester (IU 7) 2017
---
+++ palindromes/palindromes.c
@@ -0,0 +1,35 @@
+﻿#include "stdafx.h" //for Microsoft Visual Studio
+
+bool PaliCheck(int num);
+
+int main()
+{
+	int min, max;
+	int out;
+	//Запрашиваем минимальную и максимальную границу интервала
+	printf("Please enter min and max: ");
+	scanf("%i %i", &min, &max);
+	out = min;
+
+	//Проверяем все числа в указанном интервале на принадлежность к палиндромам с помощью функции.
+	for (; out < max; out++)
+		if (PaliCheck(out)) {
+			printf("\n %i", out);
+		}
+	
+	return 0;
+}
+
+//Функция для проверки: является ли переданное ей число палиндромом. Возвращает истину или ложь.
+bool PaliCheck(int num) {
+	const int D = 10;
+	int rev_num(0);
+	int temp_num = num;
+
+	do {
+		rev_num = rev_num*D + temp_num%D;
+	} while (temp_num /= D);
+
+	if (rev_num == num) return true;
+	else return false;
+}
